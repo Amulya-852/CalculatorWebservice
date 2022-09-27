27a119f36ca0c9e34cd248e02cc2e0f7dd994f59
Amulya <95533897+Amulya-852@users.noreply.github.com>
2022-09-27 08:13:42 +0530

    Update Calulatorresource.java

--- Calulatorresource.java
+++ Calulatorresource.java
@@ -2,7 +2,7 @@ package Calulatorresource;
 public calss Calculator{
     public static void main(string[] args){
         CalculatorResourceService service= new CalculatorResourceService();
-        CalculatorResource resource= service.get CalculatorResourcePort();
+        CalculatorResource resource= service.getCalculatorResourcePort();
         System.out.println(resource.add(3.4,4.5));
         System.out.println(resource.sub(5.6,2.5));
         System.out.println(resource.mul(8.1,3.1));
